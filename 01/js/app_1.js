b2b2b0230cf85f7abd951a0221b49a958a198d2a
jose luis condori <jose.l.condori@gmail.com>
2025-02-03 01:56:07 -0300

    subir carpeta 01,02,03

---
+++ 01/js/app_1.js
@@ -0,0 +1,1 @@
+console.log('soy el archivos js desde el head')
---
+++ 02/js/app.js
@@ -0,0 +1,19 @@
+console.log('Hola mundo 🛒')
+console.log("Hola mundo ✨")
+console.log('%c¡Estos son estilos!','color:blue; font-size:20px' )
+
+console.count('se ejecuto')
+console.count('se ejecuto')
+console.count('se ejecuto')
+console.count('se ejecuto')
+
+console.group("grupo de mensajes")
+console.log('Hola mundo')
+console.log('error')
+console.log('solucion')
+console.groupEnd()
+console.log('estoy afuera')
+
+
+console.warn('esto es una advertencia')
+console.error('es un error en el codigo')
